7874e1a77d8b6bf8b1edce8cdfd799549ebd7b70
Durga prasad <Tirumaladurgaprasad@gmail.com>
2024-01-11 22:55:50 +0530

    Mini Project

---
+++ viit_placement/Homepage/home.php
@@ -0,0 +1,137 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--favicon-->
+        <link rel="shortcut icon" href="favicon.jpg" type="image/icon">
+        <link rel="icon" href="favicon.jpg" type="image/icon">
+        <title>Vignan Home</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Footer -->
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body id="page-top">
+
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background:black">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="http://vignaniit.edu.in/">Vignan's institute of information technology</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../SProfile/login.php"">Login</a>
+                        </li>
+                       
+                    </ul>
+
+            </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner">
+                    <h1>PLACEMENT MANAGEMENT SYSTEM</h1>
+                    <hr>
+                    <p>We are here to Build your Skills and Career with our Driven Passion and Reality.</br>Click Below to Get Our Current Drive
+                        Details</p>
+                    <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
+                </div>
+            </div>
+        </header>
+        <div class="footer">
+			<div class="container">
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>NAVIGATION</h3>
+					<ul>
+						<li>
+							<a href="../Homepage/home.php"">Home</a>
+						</li>
+						<li>
+							<a href="../SProfile/login.php"">Student Login</a>
+						</li>
+						<li>
+							<a href="../PProfile/login.php"">Placement Login</a>
+						</li>
+						<li>
+							<a href="../HODProfile/login.php"">HOD Login</a>
+						</li>
+						<li>
+							<a href="../PriProfile/login.php"">Administrative Login</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>MEMBERS</h3>
+					<ul>
+						<li>
+							<a href="../Drives/mail.php">Customer Support</a>
+						</li>
+						<li>
+							<a href="../Drives/mail.php">Placement Support</a>
+						</li>
+						<li>
+							<a href="../Drives/mail.php">Faculty Support</a>
+						</li>
+						<li>
+							<a href="../Drives/products.php">Registered Companies"</a>
+						</li>
+						<li>
+							<a href="https://vignaniit.edu.in/training.php">Registered Companies">Training</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 get_in_touch ftr">
+					<h3>GET IN TOUCH</h3>
+					<p>VIGNAN</p>
+					<p>Duvada-Vishakapatnam
+andhra pradesh</p>
+					<p>0891-2755222/333/444/555
+
+
+</p>
+					<a href="mailto:vignaniit@yahoo.com">vignaniit@yahoo.com</a>
+				</div>
+				<div class="col-md-3 ftr-logo">
+					<p>Copyright &copy; 2022 Vignan | Developed by
+              <a href="http://znumerique.azurewebsites.net" target="_parent">MCA</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+    </body>
+
+</html>
